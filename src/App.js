8f2b9f92d20904809abb4a83edbe533756995a76
Edgar Britez <edgarfbritez@gmail.com>
2021-09-18 17:16:42 -0300

    add a header mobile version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
-import "./App.css";
+import moon from "./moon-regular.svg";
+import styles from "./App.module.css";
 import Card from "./components/Card";
+
+console.log(moon);
 const HARD_DATA = [
   {
     name: "Argentina",
@@ -19,18 +22,27 @@ const HARD_DATA = [
 ];
 function App() {
   return (
-    <div className="App">
-      {HARD_DATA.map((c) => {
-        return (
-          <Card
-            name={c.name}
-            population={c.population}
-            region={c.region}
-            capital={c.capital}
-            img={c.imgSrc}
-          />
-        );
-      })}
+    <div className={styles.App}>
+      <header className={styles.head}>
+        <h1>Where in the world?</h1>
+        <div className={styles.switcher}>
+          <img src={moon} alt="A nice moon" />
+          Dark mode
+        </div>
+      </header>
+      <main>
+        {HARD_DATA.map((c) => {
+          return (
+            <Card
+              name={c.name}
+              population={c.population}
+              region={c.region}
+              capital={c.capital}
+              img={c.imgSrc}
+            />
+          );
+        })}
+      </main>
     </div>
   );
 }
