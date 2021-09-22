558cb9d7d0fa73f4f9e9be6ab16b03b17413bb1a
Edgar Britez <edgarfbritez@gmail.com>
2021-09-21 23:13:17 -0300

    filter by country name works! Next filter by region... thinking about how to manage the states...

--- src/App.js
+++ src/App.js
@@ -14,6 +14,23 @@ function App() {
   const [firstTenCountries, setFirstTenCountries] = React.useState([]);
   const [countryPath, setCountryPath] = React.useState("path");
 
+  const [findCountryByName, setFindCountryByName] = React.useState([]);
+
+  const findCountryByNameHandler = (event) => {
+    let target = event.target.value;
+    let re = new RegExp(`^${target}`, "gi");
+    let finder = allCountries.filter((country) =>
+      country.name.toLowerCase().match(re)
+    );
+    setFindCountryByName([...finder]);
+  };
+
+  function onRegionValHandler(val) {
+    console.log("onRegionValHandler", val);
+    fetch(`https://restcountries.eu/rest/v2/region/${val}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
   // add or remove the classes in the App
   const themeHandler = () => {
     setTheme(() => (theme === "light" ? "dark" : "light"));
@@ -52,22 +69,42 @@ function App() {
           {/* I'm used isDark to change the path of the svg icons */}
           <Route path="/" exact>
             <div className="finder">
-              <SearchBar isDark={theme === "light" ? false : true} />
-              <FilterByRegion isDark={theme === "light" ? false : true} />
+              <SearchBar
+                isDark={theme === "light" ? false : true}
+                onFindCountryByName={findCountryByNameHandler}
+              />
+              <FilterByRegion
+                onRegionVal={onRegionValHandler}
+                isDark={theme === "light" ? false : true}
+              />
             </div>
             <section className="content">
-              {firstTenCountries.map((c) => {
-                return (
-                  <Card
-                    key={c.name}
-                    name={c.name}
-                    population={c.population}
-                    region={c.region}
-                    capital={c.capital}
-                    img={c.flag}
-                  />
-                );
-              })}
+              {findCountryByName.length === 0 &&
+                allCountries.map((c) => {
+                  return (
+                    <Card
+                      key={c.name}
+                      name={c.name}
+                      population={c.population}
+                      region={c.region}
+                      capital={c.capital}
+                      img={c.flag}
+                    />
+                  );
+                })}
+              {findCountryByName.length > 0 &&
+                findCountryByName.map((c) => {
+                  return (
+                    <Card
+                      key={c.name}
+                      name={c.name}
+                      population={c.population}
+                      region={c.region}
+                      capital={c.capital}
+                      img={c.flag}
+                    />
+                  );
+                })}
             </section>
           </Route>
 
--- src/components/FilterByRegion.js
+++ src/components/FilterByRegion.js
@@ -3,6 +3,15 @@ import styles from "./FilterByRegion.module.css";
 import arrow from "../images/chevron-down-solid.svg";
 import arrowBlack from "../images/chevron-down-solid-black.svg";
 
+const values = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+function Option(props) {
+  return (
+    <option onClick={() => props.onRegionVal(props.val)} value={props.val}>
+      {props.val}
+    </option>
+  );
+}
+
 function FilterByRegion(props) {
   const [isClicked, setIsClicked] = React.useState(false);
   const clickArrowHandler = () => {
@@ -11,11 +20,14 @@ function FilterByRegion(props) {
   //   TODO =>>>> add function to handler the options and filter it
   const options = (
     <div className={styles.options}>
-      <option value="Africa">Africa</option>
+      {values.map((region) => (
+        <Option val={region} onRegionVal={props.onRegionVal} />
+      ))}
+      {/* <option value="Africa">Africa</option>
       <option value="America">America</option>
       <option value="Asia">Asia</option>
       <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
+      <option value="Oceania">Oceania</option> */}
     </div>
   );
   return (
